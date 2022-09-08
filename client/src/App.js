6f5fe7f7c67d52ba45843b4f12ffc8ea7f0c4fbc
ML-std <ML-std@github. com>
2022-09-08 13:59:02 +0300

    Minor Changes On Client.

--- client/src/App.js
+++ client/src/App.js
@@ -17,10 +17,10 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+           Basic Fibonnaci Calculator
           </a>
           <Link to="/">Home</Link>
-          <Link to="/otherpage">Other Page</Link>
+          <Link to="/otherpage">Working On Page....</Link>
         </header>
         <div>
           <Routes>
