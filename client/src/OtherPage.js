4e283c75919ef859b27ea8b2d7036181bf2bfbef
ML-std <ML-std@github. com>
2022-09-08 09:06:25 +0300

    first commit

---
+++ client/src/OtherPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Link } from 'react-router-dom';
+
+export default () => {
+    return (
+    <div>
+        To go The App, please 
+        <Link to="/"> This Link</Link>
+    </div>
+    );
+};
