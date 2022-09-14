6f887b42179f86b0497d9388a671205ebe8c2245
ML-std <ML-std@github. com>
2022-09-14 15:44:48 +0300

    fix tries #2

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-           Basic Fibonnaci Calculator with K8S!
+           Basic Fibonnaci Calculator with K8S! VERSION # 2
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Working On Page....</Link>
